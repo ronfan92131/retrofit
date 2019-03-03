ee23ad324495bdc1c765134229a143214daf4f59
Ron Fan <ronfan92131@gmail.com>
2019-03-02 19:40:06 -0800

    Update JsonPlaceHolderApi.java

--- app/src/main/java/com/doyen/fans/retrofit/JsonPlaceHolderApi.java
+++ app/src/main/java/com/doyen/fans/retrofit/JsonPlaceHolderApi.java
@@ -8,5 +8,25 @@ import retrofit2.http.GET;
 public interface JsonPlaceHolderApi {
 
     @GET("posts")
-    Call<List<Post>> getPosts();
+    Call<List<Post>> getPosts(
+            @Query("userId") Integer[] userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
+
+    @GET
+    Call<List<User>> getUsers(@Url String url);
+
+
+    @GET
+    Call<List<Granular>> getGranular(@Url String url);
 }
